d3deedd504541bc14241ea4f9d2ebf294079bb0b
AviAcharya1 <138589649+AviAcharya1@users.noreply.github.com>
2024-07-15 02:19:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ["https://recipe-blog-ui.vercel.app"],
+  origin: 'https://recipe-blog-ui.vercel.app',
   methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
